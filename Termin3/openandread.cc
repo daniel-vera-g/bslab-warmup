858d9b6b9592aaeac60593739347e599f96d90f0
danielVg <danielveragi@gmail.com>
2020-10-18 23:11:42 +0200

    Open close files

--- Termin3/openandread.cc
+++ Termin3/openandread.cc
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+#include <fcntl.h>    /* For O_RDWR */
+#include <unistd.h>   /* For open(), creat() */
 
 int main(int argc, char** argv) {
 
-	std::cout << "Hello world!" << std::endl;
+  int fd; // File descriptor
 
-	return(EXIT_SUCCESS);
+  /* Open file */
+  fd = open("loreipsum.txt", O_RDONLY);
+  if(fd < 0) { // Handle errors
+    perror("ERROR: File could not be opened!");
+    exit(1);
+  }
+  std::cout<<"File for file descriptor = "<<fd<<" opened. \n";
+
+  /* Close file */
+  close(fd);
+
+  return(EXIT_SUCCESS);
 }
